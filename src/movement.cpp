83b91d1b9cbdaa59c3df1d9e5e119c2b86a17f52
NightM7 <134926839+NightM7@users.noreply.github.com>
2023-08-09 21:01:40 +0300

    aa

--- src/movement.cpp
+++ src/movement.cpp
@@ -3,19 +3,19 @@
 #include "movement.hpp"
 #include "characters.hpp"
 
-const float playerSpeed = 200.0f;
+// const float playerSpeed = 200.0f;
 
- void movement() {
-    if (IsKeyDown(KEY_W)) {
-        coords.y -= playerSpeed * GetFrameTime();
-        }
-    if (IsKeyDown(KEY_S)) {
-        coords.y += playerSpeed * GetFrameTime();
-        }
-    if (IsKeyDown(KEY_D)) {
-        coords.x += playerSpeed * GetFrameTime();
-        }
-    if (IsKeyDown(KEY_A)) {
-        coords.x -= playerSpeed * GetFrameTime();
-        }
-  }
+//  void movement() {
+//     if (IsKeyDown(KEY_W)) {
+//         coords.y -= playerSpeed * GetFrameTime();
+//         }
+//     if (IsKeyDown(KEY_S)) {
+//         coords.y += playerSpeed * GetFrameTime();
+//         }
+//     if (IsKeyDown(KEY_D)) {
+//         coords.x += playerSpeed * GetFrameTime();
+//         }
+//     if (IsKeyDown(KEY_A)) {
+//         coords.x -= playerSpeed * GetFrameTime();
+//         }
+//   }
